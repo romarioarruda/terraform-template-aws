80c7697b0a8300548d9109c5534785a8d863b3a2
Romário Arruda <35845386+romarioarruda@users.noreply.github.com>
2022-05-16 10:48:18 -0300

    Update
    Adicionando config para volume de disco e ip elastico

--- main.tf
+++ main.tf
@@ -3,42 +3,30 @@ provider "aws" {
   region = "sa-east-1"
 }
 
-provider "aws" {
-  alias = "us-east-1"
-  region = "us-east-1"
-}
-
 resource "aws_instance" "dev-sa" {
   provider = aws.sa-east-1
   # count = 1
   ami = var.amis["sa-east-1"]
   instance_type = var.instance_type
   key_name = var.key_name
+  
+  root_block_device {
+    volume_size = 60 //disk volume in GB
+  }
+  
+  user_data = file("user-data/ubuntu_server.sh")
+  
+  vpc_security_group_ids = [
+    aws_security_group.acesso_ssh-sa-east-1.id,
+    aws_security_group.acesso_web-sa-east-1.id
+  ]
+  
   tags = {
     "Name" = var.instance_tags
   }
-  vpc_security_group_ids = [aws_security_group.acesso_ssh-sa-east-1.id, aws_security_group.acesso_web-sa-east-1.id]
-  user_data = file("user-data/ubuntu_server.sh")
 }
 
-resource "aws_instance" "dev-us" {
-  provider = aws.us-east-1
-  # count = 1
-  ami = var.amis["us-east-1"]
-  instance_type = var.instance_type
-  key_name = var.key_name
-  tags = {
-    "Name" = var.instance_tags
-  }
-  vpc_security_group_ids = [aws_security_group.acesso_ssh-us-east-1.id, aws_security_group.acesso_web-us-east-1.id]
-  user_data = file("user-data/ubuntu_server.sh")
-}
-
-resource "aws_s3_bucket" "dev-sa" {
-  bucket = "teste-dev-sa"
-  acl = "private"
-
-  tags = {
-    Name = "teste-dev-sa"
-  }
+resource "aws_eip_association" "dev-sa-eip" {
+  instance_id   = aws_instance.dev-sa.id
+  public_ip = var.public_ip
 }
